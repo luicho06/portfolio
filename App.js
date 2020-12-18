51a1583903300ad6d2632434c575a8cfcd3c6df3
luicho06 <40962190+luicho06@users.noreply.github.com>
2020-12-18 16:33:18 -0500

    Update and rename src/App.js to App.js

--- App.js
+++ App.js
@@ -1,19 +1,19 @@
 import React from "react";
 import {Route, Switch} from 'react-router-dom';
 import {BrowserRouter} from 'react-router-dom'
-import MyNavbar from "./components/my-navbar/my-navbar.component";
-import MyCarousal from "./components/my-carousal/my-carousal.component";
-import MyTitleMessage from "./components/title-message/title-message.component";
-import About from "./pages/about/about.component";
-import Skills from "./pages/skills/skills.component";
+import MyNavbar from ".src/components/my-navbar/my-navbar.component";
+import MyCarousal from ".src/components/my-carousal/my-carousal.component";
+import MyTitleMessage from ".src/components/title-message/title-message.component";
+import About from ".src/pages/about/about.component";
+import Skills from ".src/pages/skills/skills.component";
 import Container from "react-bootstrap/Container";
-import TimeLine from "./components/projects-timeline/projects-timeline.component";
-import Experience from "./pages/experience/experience.component";
+import TimeLine from ".src/components/projects-timeline/projects-timeline.component";
+import Experience from ".src/pages/experience/experience.component";
 import Fade from "react-reveal/Fade";
 import Slide from "react-reveal/Slide";
-import ContactForm from "./pages/contact-form/contact-form.component";
+import ContactForm from ".src/pages/contact-form/contact-form.component";
 import { Parallax } from "react-parallax";
-import FooterPanel from "./components/footer/footer.component";
+import FooterPanel from ".src/components/footer/footer.component";
 
 import "./App.css";
 
